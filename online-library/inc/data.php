c8f0e03cc4c642dc35341de3b65a174c42a7e28b
cjsosa13 <58793660+cjsosa13@users.noreply.github.com>
2019-12-11 19:13:22 -0500

    Add files via upload

---
+++ online-library/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Night Manager",
+	"img" => "img/media/thenightmanager.jpg",
+    "genre" => "spy fiction",
+    "format" => "Paperback",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "John Le Carre"
+    ],
+    "publisher" => "Ballantine Books",
+    "isbn" => '978-1524796952'
+];
+$catalog[102] = [
+    "title" => "The War of the Worlds",
+    "img" => "img/media/thewaroftheworlds.jpg",
+    "genre" => "Scientific romance",
+    "format" => "digital",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "H.G Wells"
+    ],
+    "publisher" => "Waldman Publishing Corp",
+    "isbn" => '1603400729'
+];
+$catalog[103] = [
+    "title" => "The Wizard of Oz",
+    "img" => "img/media/thewizardofoz.jpg",
+    "genre" => "Adventure",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Frank L. Baum"
+    ],
+    "publisher" => "Puffin Books",
+    "isbn" => '9780141321028'
+];
+$catalog[104] = [
+    "title" => "Harry Potter and the Deathly Hallows",
+    "img" => "img/media/harrypotterdh.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2009,
+    "category" => "Books",
+    "authors" => [
+        "J.K Rowling"
+    ],
+    "publisher" => "Arthur A. Levine Books",
+    "isbn" => '0545139708'
+];
+//Movies
+$catalog[201] = [
+    "title" => "2012",
+    "img" => "img/media/2012.jpg",
+    "genre" => "Action",
+    "format" => "DVD/Streaming",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Roland Emmerich",
+    "writers" => [
+        "Roland Emmerich",
+
+    ],
+    "stars" => [
+        "John Cusack",
+        "Chiwetel Ejiofor",
+
+    ]
+];
+$catalog[202] = [
+    "title" => "Al Pacino Scarface",
+    "img" => "img/media/scarface.jpg",
+    "genre" => "action",
+    "format" => "DVD/Streaming",
+    "year" => 1983,
+    "category" => "Movies",
+    "director" => "Brian De Palma",
+    "writers" => [
+        "Oliver Stone",
+				"Christopher Nolan"
+    ],
+    "stars" => [
+			"Al Pacino",
+			"Steven Bauer",
+			"Michelle Pfeiffer"
+    ]
+];
+$catalog[203] = [
+    "title" => "Training Day",
+    "img" => "img/media/trainingday.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2004,
+    "category" => "Movies",
+    "director" => "Antoine Fuqua",
+    "writers" => [
+        "David Ayer",
+        "Lionel Wigram"
+    ],
+    "stars" => [
+			"Denzel Washington",
+			"Ethan Hawke",
+			"Scott Glenn"
+    ]
+];
+$catalog[204] = [
+    "title" => "The Wolf Of Wall Street",
+		"img" => "img/media/thewolfofwallstreet.jpg",
+	  "genre" => "Adventure",
+	  "format" => "DVD/Streaming",
+	  "year" => 2014,
+	  "category" => "Movies",
+	  "director" => "Martin Scorsese",
+	  "writers" => [
+	      "Terence Winter"
+
+	  ],
+	  "stars" => [
+	      "Leonardo DiCarprio",
+	      "Jonah Hill",
+
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Sorry 4 The Weight",
+	"img" => "img/media/sorry4theweight.jpg",
+	"genre" => "rap",
+	"format" => "CD",
+	"year" => 2013,
+	"category" => "Music",
+	"artist" => "Chief Keef"
+];
+
+$catalog[302] = [
+	"title" => "So Much Fun",
+	"img" => "img/media/somuchfun.jpg",
+	"genre" => "rap",
+	"format" => "Streaming Services",
+	"year" => 2019,
+	"category" => "Music",
+	"artist" => "Young Thug"
+];
+$catalog[303] = [
+	"title" => "Drip Harder",
+	"img" => "img/media/dripharder.jpg",
+	"genre" => "rap",
+	"format" => "Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Lil Baby"
+];
+$catalog[304] = [
+	"title" => "Astroworld",
+	"img" => "img/media/astroworld.jpg",
+	"genre" => "rap",
+	"format" => "Streaming Services",
+	"year" => 2017,
+	"category" => "Music",
+	"artist" => "Travis Scott"
+];
+?>
